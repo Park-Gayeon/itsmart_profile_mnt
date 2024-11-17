2f02e0d30fbd71536bccdbd146a1537c5ecf59dc
parkGaYeon <gay0830@naver.com>
2024-11-17 15:35:58 +0900

    #17 항목추가/제거기능
    중간 commit > 추가 항목별로 Controller 분리하여 분기 예정

--- src/main/java/kr/co/itsmart/projectdemo/service/ProfileModifyService.java
+++ src/main/java/kr/co/itsmart/projectdemo/service/ProfileModifyService.java
@@ -1,29 +1,19 @@
 package kr.co.itsmart.projectdemo.service;
 
-public interface ProfileModifyService {
-    /* 프로필 history 생성 */
-    int insertUsrProfileHist(String user_id);
-    int insertUsrQualificationHist();
-    int insertUsrWorkExperienceHist();
-    int insertUsrProjectHist();
-    int inserUsrSkillHist();
+import kr.co.itsmart.projectdemo.vo.ProfileVO;
 
+public interface ProfileModifyService {
 
     /* 프로필 수정(인적사항) */
-    int updateUsrProfileInfo(String user_id);
-
-    /* 프로필 사진 file_seq update */
-    int updateUsrProfileImage();
-
-    /* 프로필 수정(자격증) */
-    int updateUsrQualification();
-
-    /* 프로필 수정(근무경력) */
-    int updateUsrWorkExperience();
+    boolean updateUsrProfileInfo(ProfileVO profileVO);
 
     /* 프로필 수정(사업경력) */
-    int updateUsrProject();
+    void updateUsrProject(ProfileVO profile);
 
-    /* 프로필 수정(기술) */
-    int updateUsrSkill();
+    /* 프로필 수정(자격증) */
+    void updateUsrQualification(ProfileVO profile);
+
+    /* 프로필 수정(근무경력) */
+    void updateUsrWorkExperience(ProfileVO profile);
+
 }
--- src/main/java/kr/co/itsmart/projectdemo/service/ProfileModifyServiceImpl.java
+++ src/main/java/kr/co/itsmart/projectdemo/service/ProfileModifyServiceImpl.java
@@ -1,7 +1,13 @@
 package kr.co.itsmart.projectdemo.service;
 
 import kr.co.itsmart.projectdemo.dao.ProfileDAO;
+import kr.co.itsmart.projectdemo.vo.ProfileVO;
+import kr.co.itsmart.projectdemo.vo.ProjectVO;
+import kr.co.itsmart.projectdemo.vo.QualificationVO;
+import kr.co.itsmart.projectdemo.vo.WorkExperienceVO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
 @Service
 public class ProfileModifyServiceImpl implements ProfileModifyService{
@@ -12,57 +18,91 @@ public class ProfileModifyServiceImpl implements ProfileModifyService{
     }
 
     @Override
-    public int insertUsrProfileHist(String user_id) {
-        return 0;
+    @Transactional
+    public boolean updateUsrProfileInfo(ProfileVO profileVO) {
+        try {
+            // UPDATE PROFILE INFO
+            boolean update = profileDAO.updateUsrProfileInfo(profileVO);
+
+            // CREATE HIST
+            boolean create = profileDAO.insertUsrProfileInfoHist(profileVO);
+
+            return update && create;
+        } catch (Exception e){
+            System.out.println("updateUsrProfileInfo 실패: " + e.getMessage());
+            return false;
+        }
     }
 
     @Override
-    public int insertUsrQualificationHist() {
-        return 0;
+    @Transactional
+    public void updateUsrProject(ProfileVO profile) {
+        // DELETE TB
+        String user_id = profile.getUser_id();
+        int hist_seq = profile.getHist_seq();
+        profileDAO.deleteUsrProjectInfo(user_id);
+
+        for(ProjectVO project : profile.getProjectList()){
+            try {
+                project.setUser_id(user_id);
+                project.setHist_seq(hist_seq);
+                // UPDATE(=INSERT) TB
+                profileDAO.updateUsrProjectInfo(project);
+                // CREATE HIST
+                profileDAO.insertUsrProjectInfoHist(project);
+            } catch (Exception e){
+                System.out.println("updateUsrProject 실패: " + e.getMessage());
+                TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+                throw e;
+            }
+        }
     }
 
     @Override
-    public int insertUsrWorkExperienceHist() {
-        return 0;
+    @Transactional
+    public void updateUsrQualification(ProfileVO profile) {
+        // DELETE TB
+        String user_id = profile.getUser_id();
+        int hist_seq = profile.getHist_seq();
+        profileDAO.deleteUsrQualificationInfo(user_id);
+
+        for(QualificationVO qualification : profile.getQualificationList()){
+            try{
+                qualification.setUser_id(user_id);
+                qualification.setHist_seq(hist_seq);
+                // UPDATE(=INSERT) TB
+                profileDAO.updateUsrQualificationInfo(qualification);
+                // CREATE HIST
+                profileDAO.insertUsrQualificationInfoHist(qualification);
+            } catch (Exception e){ // try-catch 문이 있는 경우 @Transaction rollback 이 적용 되지 않을 수 있어 수동 으로 rollback
+                System.out.println("updateUsrQualification 실패: " + e.getMessage());
+                TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+                throw e;
+            }
+        }
     }
 
     @Override
-    public int insertUsrProjectHist() {
-        return 0;
-    }
+    @Transactional
+    public void updateUsrWorkExperience(ProfileVO profile) {
+        // DELETE TB
+        String user_id = profile.getUser_id();
+        int hist_seq = profile.getHist_seq();
+        profileDAO.deleteUsrWorkInfo(user_id);
 
-    @Override
-    public int inserUsrSkillHist() {
-        return 0;
-    }
-
-    @Override
-    public int updateUsrProfileInfo(String user_id) {
-        return 0;
-    }
-
-    @Override
-    public int updateUsrProfileImage() {
-        return 0;
-    }
-
-    @Override
-    public int updateUsrQualification() {
-        return 0;
-    }
-
-    @Override
-    public int updateUsrWorkExperience() {
-        return 0;
-    }
-
-    @Override
-    public int updateUsrProject() {
-        return 0;
-    }
-
-    @Override
-    public int updateUsrSkill() {
-        return 0;
+        for(WorkExperienceVO work : profile.getWorkExperienceList()){
+            try {
+                work.setUser_id(user_id);
+                work.setHist_seq(hist_seq);
+                // UPDATE(=INSERT) TB
+                profileDAO.updateUsrWorkInfo(work);
+                // CREATE HIST
+                profileDAO.insertUsrWorkInfoHist(work);
+            } catch (Exception e){
+                System.out.println("updateUsrWorkExperience 실패: " + e.getMessage());
+                TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+                throw e;
+            }
+        }
     }
 }
