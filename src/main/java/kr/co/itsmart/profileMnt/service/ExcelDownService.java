5a518881b8d1ab17aa9f110cb6d0038e48f17481
parkGaYeon <gay0830@naver.com>
2024-12-20 01:34:51 +0900

    excel download - infoList
    [관리자]직원프로필 관리 목록 페이지 엑셀 다운로드

--- src/main/java/kr/co/itsmart/profileMnt/service/ExcelDownService.java
+++ src/main/java/kr/co/itsmart/profileMnt/service/ExcelDownService.java
@@ -4,7 +4,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import kr.co.itsmart.profileMnt.vo.ProfileVO;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface ExcelDownService {
     void downloadUsrProfileDetailExcel(ProfileVO profile, HttpServletResponse response) throws IOException;
+
+    void downloadUsrProfileAllListExcel(List<ProfileVO> list, HttpServletResponse response) throws IOException;
 }
